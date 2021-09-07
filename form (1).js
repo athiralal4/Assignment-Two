fcefd42df93bc878817e6510273e846447f862fc
athiralal4 <31566942+athiralal4@users.noreply.github.com>
2021-09-07 10:13:29 +0530

    Update form (1).js

--- form (1).js
+++ form (1).js
@@ -1,6 +1,4 @@
 const subButton = document.querySelector("#submit"); 
-
-
 subButton.addEventListener("click", function(){
 
     window.location.href="thankyou.html";
@@ -16,5 +14,36 @@ function ckChange(el) {
   }
 
 
+function ckDisable(el){
+    var noSafety=document.getElementById("check13");
+    
+    if(el.checked)
+    {
+        if( !noSafety.disabled)
+        {    
+            noSafety.disabled = true;
+            
+        }
+    }
+    else{
+        if(noSafety.disabled== true)
+        {
+            var flag=1;
+        var ckName=document.getElementsByClassName("checkbox");
+        for (var i = 0, c; c = ckName[i]; i++)
+        {
+            if(c.checked)
+            {
+                flag=0;
+            }
+        }
+        if (flag==0){
+            noSafety.disabled=true;
+        }
+        else{
+            noSafety.disabled=false;
+        }
 
-   
+    }
+    }
+}
