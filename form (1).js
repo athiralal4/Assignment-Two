86a101557013948692f2e90a561625c5b16dfaeb
athiralal4 <31566942+athiralal4@users.noreply.github.com>
2021-09-15 02:00:13 +0530

    Update form (1).js

--- form (1).js
+++ form (1).js
@@ -1,49 +1,139 @@
-const subButton = document.querySelector("#submit"); 
-subButton.addEventListener("click", function(){
+const formId = "save-later-form"; 
+const url = location.href; 
+const formIdentifier = `${url} ${formId}`; 
+const saveButton = document.querySelector("#save"); 
+const subButton = document.querySelector("#submit1"); 
+let form = document.querySelector(`#${formId}`); 
+let formElements = form.elements; 
 
-    window.location.href="thankyou.html";
-});
+//Getdata
+const getFormData = () => {
+  let data = { [formIdentifier]: {} };
+  for (const element of formElements)
+	{if (element.name.length > 0 && element.name != "gender") 
+		{ data[formIdentifier][element.name] = element.value;}
+	}return data;
+  };
 
-function ckChange(el) {
-    var ckName = document.getElementsByName(el.name);
 
-    for (var i = 0, c; c = ckName[i]; i++)
- {
-     c.disabled = !(!el.checked || c === el);
-    }
+function checkEmail(){
+  var email= document.getElementById("email");
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if(email.value.match(mailformat))
+  {return true;
+  }
+  else
+  {
+  alert("You have entered an invalid email address!");
+  return false;
+  }
+  
+}
+
+function checkPass(){
+  var pass = document.getElementById("pass");
+  var passformat = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{4,}$/;
+  if(pass.value.match(passformat))
+  {return true;
+  }
+  else
+  {
+  alert("You have entered an invalid password!");
+  return false;
+  }
+  
+}
+
+function checkMob(){
+  var mob = document.getElementById("mob");
+  var mobformat = /(7|8|9)\d{9}/;
+  if(mob.value.match(mobformat))
+  {return true;
+  }
+  else
+  {
+  alert("You have entered an invalid mobile number!");
+  return false;
   }
 
 
-function ckDisable(el){
-    var noSafety=document.getElementById("check13");
-    
-    if(el.checked)
-    {
-        if( !noSafety.disabled)
-        {    
-            noSafety.disabled = true;
-            
-        }
-    }
-    else{
-        if(noSafety.disabled== true)
-        {
-            var flag=1;
-        var ckName=document.getElementsByClassName("checkbox");
-        for (var i = 0, c; c = ckName[i]; i++)
-        {
-            if(c.checked)
-            {
-                flag=0;
-            }
-        }
-        if (flag==0){
-            noSafety.disabled=true;
-        }
-        else{
-            noSafety.disabled=false;
-        }
-
-    }
-    }
 }
+
+function checkValidate(){
+  var flag=0;
+  for (const element of formElements)
+  {  
+    if (element.value.length ==0 && element.name != "submit" && element.name!="reset" && element.name!="gender"){
+      console.log(element.name);
+
+      alert(element.name +" is missing!");
+      flag=1;
+    } }
+      
+      var radios=document.getElementsByName("gender");
+     
+      if (!(radios[0].checked || radios[1].checked || radios[2].checked)) {
+          alert("Gender is missing");
+          flag=1;
+      } 
+  if(flag==0){
+    return true;
+  }else{
+    return false;
+  }
+}
+  
+//Onsubmit
+subButton.addEventListener("click", function()
+ {
+  if (checkValidate()==true ) {
+  
+     if(checkEmail()==true && checkPass()==true && checkMob()==true){
+       console.log("true");
+  
+	if (saveButton.checked == true)
+  { 
+    event.preventDefault();
+    data = getFormData();
+    localStorage.setItem(formIdentifier, JSON.stringify(data[formIdentifier]));   
+   
+   const radios =  document.getElementsByName("gender");
+           for(var i =0; i<radios.length;i++)
+		{
+            if (radios[i].checked == true)
+			{
+			   localStorage.setItem('gender',radios[i].value);
+            }
+        }window.location.href="thankyou.html";
+  }
+  else
+   { localStorage.clear(); }
+}}
+else{
+  event.preventDefault();
+}
+ });
+   
+   
+
+//onload
+const populateForm = () => {
+	if (localStorage.key(formIdentifier)) {
+    const savedData = JSON.parse(localStorage.getItem(formIdentifier)); 
+    for (const element of formElements) {
+      if (element.name in savedData) {
+        element.value = savedData[element.name];
+		}
+    }}
+	
+  	var radios = document.getElementsByName("gender");
+    var a = localStorage.getItem('gender');
+    for(var i=0;i<radios.length;i++){
+        if(radios[i].value == a){
+            radios[i].checked = true; 
+  }
+}
+};
+
+document.onload = populateForm(); 
+  
